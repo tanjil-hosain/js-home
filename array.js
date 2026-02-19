0dec43c2c0d9e4d2dcaf4c7a2a34407f9c6c0f3d
tanjil-hosain <tanjilh85@gmail.com>
2026-02-19 23:30:14 +0600

    array,push,unshift

---
+++ array.js
@@ -0,0 +1,10 @@
+let student = ["Rakib","Shakib","Akib","fahim","Said","Tanjil"];
+console.log(student);
+console.log([3]);   //er index number er maddhome specic item pawa jay.
+console.log(typeof student)   // typeof er maddhome data type jana jay
+
+let student2=["Rakib","Shakib","Akib","fahim","Said","Tanjil"];
+student2.push("Saif");  // push er maddome kono item k last add kora jay
+console.log(student2);
+student2.unshift("Rakibbaaa");
+console.log(student2);

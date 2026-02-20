0308ebd79202f193a284c4ef4856e5906ce80436
tanjil-hosain <tanjilh85@gmail.com>
2026-02-20 16:13:26 +0600

    IF Statement

---
+++ Statement/if-statement.js
@@ -0,0 +1,16 @@
+// if(condition){
+//             //STATEMENT        <!--Sudu true ta exist hoy-->
+//         }
+//if statement sudu ekta resullt dekhay
+let a=7;
+let b=20;
+if(a<b){
+    console.log("apnar order ti succes"); 
+}
+//output e sudu true hole dekhabe na hoy na
+
+let m = 50;
+let n = 60;
+if(n>m){
+    console.log("hey Apnar formula ta sotti");
+}

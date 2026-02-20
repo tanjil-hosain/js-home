0087ad12b40d2adc1c9c5c2ed59d2ad23dc2b962
tanjil-hosain <tanjilh85@gmail.com>
2026-02-20 17:22:18 +0600

    nasted statement

---
+++ Statement/nasted_statment.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+let marks=Number(prompt("Plaese Enter Your Marks:"));
+if(marks>100){
+    console.log("This is Invalid Marks");
+}else if(marks>=80){
+    console.log("A+");
+} else if(marks>=70){
+    console.log("A");
+}  else if(marks>=60){
+    console.log("A-");
+} else if(marks>=50){
+    console.log("B");
+}else if(marks>=40){
+    console.log("C");
+} else if (marks>=33){
+    console.log("D");
+}else{
+    console.log("F");
+}

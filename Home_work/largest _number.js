256c23020fdbf352a19ec5ca3958934d9accbe21
tanjil-hosain <tanjilh85@gmail.com>
2026-02-21 23:59:48 +0600

    largest number

--- Home_work/largest _number.js
+++ Home_work/largest _number.js
@@ -12,3 +12,14 @@ if(num1>num2 && num1>num3){
     largest=num3;
 } 
 console.log(largest);
+
+//2nd method
+let numbers =[]; 
+let total_input=Number(prompt("Enter Your Input Number:"));
+ for(let i =0; i < total_input; i++){
+let input= Number(prompt(`Enter your ${i+1}nd Number: `))
+numbers.push(input);
+    
+}
+let max = Math.max(...numbers);
+console.log(`The maximum number is: ${max}`);

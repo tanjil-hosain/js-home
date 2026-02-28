f5864653adc620c6421eac3e145b2991cce594d9
tanjil-hosain <tanjilh85@gmail.com>
2026-02-28 23:20:44 +0600

    prime number

--- Problem/prime-number.js
+++ Problem/prime-number.js
@@ -1,7 +1,25 @@
    const prompt = require('prompt-sync')();
-   let num =Number(prompt("Input your Number")) ;
+//    let num =Number(prompt("Input your Number")) ;
+//    let count = 0;
+//         if (num == 0 || num == 1) {
+//             console.log("Not Prime or Composite");
+//         } else {
+//             for (i = 2; i < num; i++) {
+//                 if (num % i == 0) {
+//                     count++;
+//                     break;
+//                 }
+//             }
+//             if (count == 0) {
+//                 console.log("Prime");
+//             } else {
+//                 console.log("not Prime");
+//             }
+//         }
+
+let num =Number(prompt("Input your Number")) ;
    let count = 0;
-        if (num == 0 || num == 1) {
+        if (num <=1) {
             console.log("Not Prime or Composite");
         } else {
             for (i = 2; i < num; i++) {

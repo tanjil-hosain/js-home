ab47d5807d0f3fe97f1f3b8f36a36be589247359
tanjil-hosain <tanjilh85@gmail.com>
2026-03-01 23:46:22 +0600

    pattern

---
+++ Function/pattern.js
@@ -0,0 +1,26 @@
+//     document.write("row"+ "<br>");
+// }
+
+
+// let rows = 5;    
+// let stars = 10;  
+
+// for (let i = 1; i <= rows; i++) {
+//     let row = ""; 
+    
+//     for (let j = 1; j <= stars; j++) {
+//         row += "* "; 
+//     }
+    
+//     console.log(row); 
+// }
+
+for (let i = 10; i > rows; i--) {
+    let row = ""; 
+    
+    for (let j = 1; j > stars; j--) {
+        row += "* "; 
+    }
+    
+    console.log(row); 
+}

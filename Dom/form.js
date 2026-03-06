89f3d81ba22f5f71907c0e6e7097dc4e7e5e5a76
tanjil-hosain <tanjilh85@gmail.com>
2026-03-06 22:13:29 +0600

    dyanamic form

--- Dom/form.js
+++ Dom/form.js
@@ -1,4 +1,4 @@
-function info(){
+ function info(){
 
 let id = document.form.id.value;
 let name = document.form.name.value;
@@ -8,26 +8,28 @@ let round = document.form.round.value;
 let gender = "";
 let subject = "";
 
-let  store = window.open("","", "width =400px, height = 400px");
+let  store = window.open("","", "width =400px , height = 400px");
 
 for(i=0; i<document.form.gender.length; i++){
     if(document.form.gender[i].checked){
-        gender= document.form.gender[i].value + ","
+        gender= document.form.gender[i].value + ""
     }
 }
 
-for(i=0, i< document.form.subject.length; i++){
-    if(document.form.subject[i].checked)
-        subject= document.form.subject[i].value + ",";
+for(i=0; i< document.form.subject.length; i++){
+    if(document.form.subject[i].checked){
+        subject+= document.form.subject[i].value + ","
+    }
 
 
 }
+   store.document.write("Stuent Id "+id + "<br>")
  store.document.write("Student Name :" + name + "<br>");
           store.document.write("Student Address :" + address + "<br>");
+          store.document.write("Email " + email +  "<br>");
           store.document.write("Round: " + round +  "<br>");
-          store.document.write("Round: " + round +  "<br>");
-          store.document.write("ID: "+subject)
-          store.document.write("Email: "+subject)
-          store.document.write("Gender: "+subject)
+          store.document.write("Gender "+gender  +  "<br>")
+          store.document.write("Subject: "+subject)
+       
           
 }

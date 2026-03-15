610a0b0002b1f49008e8c6c1bc07b045dd3269c5
tanjil-hosain <tanjilh85@gmail.com>
2026-03-15 22:59:39 +0600

    variables

---
+++ js-recape/variables/variable.js
@@ -0,0 +1,27 @@
+var name = "Tanjil";  // var redeclare kora jay abong reassing kora and etar last variable value ta sob somoy dekhabe
+var name = "tanjil";
+console.log(name);
+
+let name1 = "RAkib";  // let redeclare kora jay na kintu reassing kora jay!!
+let name2 = "akib"
+console.log(name1);
+console.log(name2);
+
+
+const myself= "Tanjil Hossain";
+// myself="tanjil"          // const fixed , redeclare and reassing kora jay na
+console.log(myself);
+
+//addition
+let num1= 10;
+let num2= 1000;
+let total = (num1+num2)+num1;
+console.log(total);
+
+//Substraction
+
+let sub1=5000;
+let sub2 = 256;
+let Substraction = sub1-sub2;
+console.log(Substraction);
+
